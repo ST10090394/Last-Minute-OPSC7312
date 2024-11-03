01395f9a263bf7c2779b987fb393e6449b8f3623
ST10090454 <108759820+jabals64@users.noreply.github.com>
2024-11-04 00:04:06 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ buildscript {
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    id("com.android.library") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
-    // Add the dependency for the Google services Gradle plugin
+    id("com.android.application") version "8.1.0" apply false // Updating Gradle Plugin to 8.1.0 for compatibility with newer Kotlin
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false // Updated Kotlin version to match dependencies
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
